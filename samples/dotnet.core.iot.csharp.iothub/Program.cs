29448851a571065506a6ef3fb2571066698bad4c
Dave Glover <dglover@microsoft.com>
2020-06-15 18:07:54 +1000

    updated to .net core 3 amd dps for Iot central

--- samples/dotnet.core.iot.csharp.iothub/Program.cs
+++ samples/dotnet.core.iot.csharp.iothub/Program.cs
@@ -1,57 +1,81 @@
 ﻿using System;
-using Iot.Device.CpuTemperature;
-using Newtonsoft.Json;
-using Microsoft.Azure.Devices.Client;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.Devices.Provisioning.Client;
+using Microsoft.Azure.Devices.Provisioning.Client.Transport;
+using Microsoft.Azure.Devices.Shared;
+using Iot.Device.CpuTemperature;
+using Newtonsoft.Json;
 
 namespace dotnet.core.iot
 {
     class Program
     {
-        const string DeviceConnectionString = "<Your Azure IoT Hub Connection String>";
+        private static string idScope = Environment.GetEnvironmentVariable("DPS_IDSCOPE");
+        private static string registrationId = Environment.GetEnvironmentVariable("DPS_REGISTRATION_ID");
+        private static string primaryKey = Environment.GetEnvironmentVariable("DPS_PRIMARY_KEY");
+        private static string secondaryKey = Environment.GetEnvironmentVariable("DPS_SECONDARY_KEY");
+        private const string GlobalDeviceEndpoint = "global.azure-devices-provisioning.net";
+
+        //const string DeviceConnectionString = "<Your Azure IoT Hub Connection String>";
 
         // Replace with the device id you used when you created the device in Azure IoT Hub
-        const string DeviceId = "<Your Device Id>";
-        static DeviceClient _deviceClient = DeviceClient.CreateFromConnectionString(DeviceConnectionString, TransportType.Mqtt);
+        //const string DeviceId = "<Your Device Id>";
+        // static DeviceClient iotClient = DeviceClient.CreateFromConnectionString(DeviceConnectionString, TransportType.Mqtt);
         static CpuTemperature _temperature = new CpuTemperature();
         static int _msgId = 0;
         const double TemperatureThreshold = 42.0;
 
+
+        // https://github.com/Azure-Samples/azure-iot-samples-csharp/blob/master/provisioning/Samples/device/SymmetricKeySample/Program.cs
+        // Azure IoT Central Client
+
         static async Task Main(string[] args)
         {
-            while (true)
+            IAuthenticationMethod auth;
+
+            using (var security = new SecurityProviderSymmetricKey(registrationId, primaryKey, secondaryKey))
+            using (var transport = new ProvisioningTransportHandlerHttp())
             {
-                if (_temperature.IsAvailable)
+                ProvisioningDeviceClient provClient = ProvisioningDeviceClient.Create(GlobalDeviceEndpoint, idScope, security, transport);
+                DeviceRegistrationResult result = await provClient.RegisterAsync();
+                auth = new DeviceAuthenticationWithRegistrySymmetricKey(result.DeviceId, (security as SecurityProviderSymmetricKey).GetPrimaryKey());
+
+                using (DeviceClient iotClient = DeviceClient.Create(result.AssignedHub, auth, TransportType.Mqtt))
                 {
-                    Console.WriteLine($"The CPU temperature is {Math.Round(_temperature.Temperature.Celsius, 2)}");
-                    await SendMsgIotHub(_temperature.Temperature.Celsius);
+                    while (true)
+                    {
+                        if (_temperature.IsAvailable)
+                        {
+                            Console.WriteLine($"The CPU temperature is {Math.Round(_temperature.Temperature.Celsius, 2)}");
+                            await SendMsgIotHub(iotClient, _temperature.Temperature.Celsius);
+                        }
+                        Thread.Sleep(2000); // sleep for 2 seconds
+                    }
                 }
-                Thread.Sleep(2000); // sleep for 2000 milliseconds
             }
         }
 
-        private static async Task SendMsgIotHub(double temperature)
+        private static async Task SendMsgIotHub(DeviceClient iotClient, double temperature)
         {
             var telemetry = new Telemetry() { Temperature = Math.Round(temperature, 2), MessageId = _msgId++ };
             string json = JsonConvert.SerializeObject(telemetry);
 
             Message eventMessage = new Message(Encoding.UTF8.GetBytes(json));
             eventMessage.Properties.Add("temperatureAlert", (temperature > TemperatureThreshold) ? "true" : "false");
-            await _deviceClient.SendEventAsync(eventMessage).ConfigureAwait(false);
+            await iotClient.SendEventAsync(eventMessage).ConfigureAwait(false);
         }
 
         class Telemetry
         {
-            [JsonPropertyAttribute (PropertyName="temperature")] 
+            [JsonPropertyAttribute(PropertyName = "Temperature")]
             public double Temperature { get; set; } = 0;
 
-            [JsonPropertyAttribute (PropertyName="messageId")] 
+            [JsonPropertyAttribute(PropertyName = "MsgId")]
             public int MessageId { get; set; } = 0;
-
-            [JsonPropertyAttribute (PropertyName="deviceId")] 
-            public string DeviceId {get; set;} = Program.DeviceId;
         }
     }
 }
